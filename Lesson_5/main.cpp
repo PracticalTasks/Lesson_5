87b75a1c1f2cac7bf5947fa45bceba9503e7806a
Alexandr <kajlaleksandr1986@gmail.com>
2021-12-07 15:29:32 +0700

    commit

--- Lesson_5/main.cpp
+++ Lesson_5/main.cpp
@@ -7,6 +7,7 @@
 #include<map>
 #include<iterator>
 #include <fstream>
+#include<string>
 
 
 using namespace std;
@@ -16,15 +17,42 @@ template<typename T>
 void printUniqueWords(T beg,T end)
 {
 	set<string> mySet;
+	//Adding a container to set
 	copy(beg, end, inserter(mySet, mySet.end()));
 	copy(mySet.begin(), mySet.end(), ostream_iterator<string>(cout, " "));
 	cout << endl;
 }
 
 //Task 2
-void userInput()
+void processingInputData(string& buf)
 {
+	
 	multimap<int,string> myMap;
+	auto item = 0;
+	auto prev = 0;
+	auto count = 0;
+	//Get a sentence from the buffer
+	for (auto smb : buf)
+	{
+		if (smb != '.' && smb != '!' && smb != '?')
+		{
+			++item;
+		}
+		else
+		{
+			//Remove the space from the beginning of the sentence
+			if (buf[prev] == ' ')
+				++prev;
+
+			count = ++item - prev;
+			//Adding a sentence to the multimap
+			myMap.insert(pair<int,string>(count, buf.substr(prev, count)));
+			prev = item;
+		}
+	}
+	
+	//Output on display
+	for_each(myMap.begin(), myMap.end(), [](pair<int, string> item) {cout << item.second <<"\n\n"; });
 }
 
 int main()
@@ -45,6 +73,18 @@ int main()
 	{
 		//Task 2
 		cout << "\nTask 2\n";
-
+		//Excerpt from the book 
+		ifstream file("Толстой Лев. Война и мир.txt");
+		if (!file)
+			cout << "File not found";
+		string buf;
+		string tmpBuf;
+		while (!file.eof())
+		{
+			getline(file, tmpBuf);
+			buf += tmpBuf;
+		}
+		
+		processingInputData(buf);
 	}
 }
